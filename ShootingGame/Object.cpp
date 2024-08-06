d006a3dde00140a9f1004c151c44543fe1a80a55
1thorn1 <kjhi4691@naver.com>
2024-08-06 10:34:48 +0900

    충돌처리랑 이동 업데이트 조건 수정

--- ShootingGame/Object.cpp
+++ ShootingGame/Object.cpp
@@ -5,7 +5,7 @@
 
 void Object::Initialize(EOBJECT_TYPE type)
 {
-	m_type = type;	
+	m_type = type;
 	if (m_type == PLAYER_BULLET || m_type == ENEMY_BULLET)
 	{
 		m_radius = BULLET_RADIUS;
@@ -16,27 +16,31 @@ bool Object::IsCollide(const Object& other)
 {
 	///////	/////////////////
 	// EXAM_03. 자신과 다른 오브젝트의 위치와 반지름을 고려하여 원과 원의 충돌검사 함수를 작성하시오
-
+	float distance = sqrt(pow(this->m_position.x - other.m_position.x, 2) + pow(this->m_position.y - other.m_position.y, 2));
+	if (distance <= this->m_radius + other.m_radius)
+	{
+		return true;
+	}
 	////////////////////////
 	return false;
 }
 
 void Object::UpdatePosition(float elapsedTimeSec)
-{	
-	Vector2 moveDirection(0.0f,0.0f);
+{
+	Vector2 moveDirection(0.0f, 0.0f);
 	if (m_inputVector != Vector2(0.0f, 0.0f))
 	{
 		m_inputVector.Normalize();
 		moveDirection = m_inputVector;
-	}	
+	}
 	/////////////////////////
 	// EXAM_04 이동방향과 속력 그리고 경과시간을 고려하여 위치를 업데이트하는 코드를 작성하시오
-
+	m_position += moveDirection * m_speedPerSec * elapsedTimeSec;
 	////////////////////////
 }
 
 void Object::Render()
-{		
+{
 	COLORREF color = RGB(255, 255, 255);
 	switch (m_type)
 	{
@@ -49,7 +53,7 @@ void Object::Render()
 		color = RGB(255, 0, 0);
 		break;
 	}
-	Render::DrawCircle((int)m_position.x, (int)m_position.y,(int)m_radius, color);
+	Render::DrawCircle((int)m_position.x, (int)m_position.y, (int)m_radius, color);
 }
 
 
